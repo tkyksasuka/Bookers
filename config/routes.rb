d32d2389bd8a21750c64a096b9dd43ac3a5737a4
tkyksasuka <110615564+tkyksasuka@users.noreply.github.com>
2022-08-29 15:43:43 +0000

    exercise2_4day

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
- root to 'homes#top'
+ get root to: 'homes#top'
  get '/books' => 'books#index'
- get '/books/:id' => 'books#show'
+ post '/books' => 'books#create'
+ get '/books/:id' => 'books#show', as: 'book'
  get '/books/:id/edit' => 'books#edit'
- 
+
+
 end
